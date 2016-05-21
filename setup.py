864c5c775fef0efb48e556daf7fb3f7e66d6be9f
Ivan Herman <ivan@ivan-herman.net>
2016-05-21 11:37:25 +0200

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='underscore_py',
+      version='1.0',
+      description='Set of utilities to make a functional-programming style usage of Python easier',
+      author='Ivan Herman',
+      author_email='ivan@ivan-herman.net',
+      url='https://github.com/iherman/underscore_py',
+      license='https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document',
+      py_modules=['underscore'],
+     )
